1fe10cfd3368cfe4f77b07fd84f46843e0aaa901
NipponPa <ohbabyittriple@yahoo.com>
2025-11-25 06:33:38 +0000

    change favicon

---
+++ supabase/migrations/05_event_reminder_schema.sql
@@ -0,0 +1,5 @@
+-- Add reminder_minutes column to events table
+ALTER TABLE public.events ADD COLUMN IF NOT EXISTS reminder_minutes INTEGER DEFAULT NULL;
+
+-- Add comment to document the purpose of the column
+COMMENT ON COLUMN public.events.reminder_minutes IS 'Number of minutes before the event start time to send a reminder notification. NULL means no reminder.';uuu
